19902b6721eede2fa81ed3532e92c08dae1a2289
shahabhusain <shahabhussain098123@gmail.com>
2025-01-14 16:14:05 +0500

    commit

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 const Layout = () => {
   return (
     <div className=' flex gap-6'>
-         <div className=''>
+         <div className=' sticky top-0 h-screen'>
          <SideBar />
          </div>
            <div className=' w-[100%] pb-12 ml-[15rem]'>
